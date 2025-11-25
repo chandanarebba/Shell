96da572dcd16fd8a1dc920a6f5bf27cdb62898c4
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 20:23:40 +0000

    shell-script

--- colour1.sh
+++ colour1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-R="\e[33m"
+R="\e[31m"
 G="\e[32m"
-Y="\e[31m"
+Y="\e[33m"
 W="\e[0m"
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
@@ -24,3 +24,16 @@ if [ $? -ne 0 ]; then
 else
     echo -e "MYSQL is already exists......$Y SKIPPING $N"
 fi
+dnf list installed nginx
+if [ $? -ne 0]; then
+   dnf install nginx -y
+   VALIDATE $? "NGINX"
+else
+   echo -e "NGINX is already exists......$Y SKIPPING $N"
+dnf list installed python3
+if [ $? -ne 0]; then
+   dnf install python3 -y
+   VALIDATE $? "python3"
+else
+   echo -e "python3 is already exists......$Y SKIPPING $N"
+
