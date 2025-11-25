ff605d131c88eb73d1e557302f9ee8f0d26be6c8
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 18:47:48 +0000

    con2

--- con_2.sh
+++ con_2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-$n=$1
+ $n=$1
 if [ $n -lt 10 ]; then
   echo "Given number $n is less than 10 "
 elif [ $n -gt 10 ]; then
    echo "Given number $n is greater than 10"
 else
    echo "Given number $n is equal to 10"
+fi
