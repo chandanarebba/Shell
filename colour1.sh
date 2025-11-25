9c39245e5bb047922eb446e9baf207583903d242
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 20:12:39 +0000

    shell-script

--- colour1.sh
+++ colour1.sh
@@ -4,12 +4,14 @@ G="\e[32m"
 Y="\e[31m"
 W="\e[0m"
 USER_ID=$(id -u)
-if [ USER_ID -ne 0 ]; then
+if [ $USER_ID -ne 0 ]; then
    echo "ERROR:Please run this script with root access"
+   exit 1
 fi
 VALIDATE(){
     if [ $1 -ne 0 ]; then
        echo "ERROR: Installing $2 is failure"
+       exit 1
     else
        echo "Installing $2 is success"
     fi
