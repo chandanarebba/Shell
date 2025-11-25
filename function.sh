db250dc00631046d25846ea6f65b9a46d2c5f6b5
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 19:25:47 +0000

    function is not equal to 1

---
+++ function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#check whether script is running with root access or not
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+   echo "ERROR:PLEASE RUN THIS SCRIPT WITH ROOT ACCESS"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "ERROR: Installing $2 is failure "
+    else
+    echo "Installing $2 is success"
+    exit 1
+    fi
+}
+dnf install mysql-server -y
+VALIDATE $? "MYSQL"
