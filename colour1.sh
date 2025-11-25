eda8ff9a98d44c62606550b5b1164cbaf0509861
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 20:10:54 +0000

    shell-script

---
+++ colour1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+R="\e[33m"
+G="\e[32m"
+Y="\e[31m"
+W="\e[0m"
+USER_ID=$(id -u)
+if [ USER_ID -ne 0 ]; then
+   echo "ERROR:Please run this script with root access"
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo "ERROR: Installing $2 is failure"
+    else
+       echo "Installing $2 is success"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql-server -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MYSQL is already exists......SKIPPING"
+fi
