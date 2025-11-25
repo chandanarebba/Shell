974d3e038d63a68787f34e7d4230505bb3cb9f16
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 18:10:11 +0000

    cool

--- if-con_1.sh
+++ if-con_1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "please enter the number"
 read n
-if[$(($n%2)) -eq 0] then
+if [$(($n%2)) -eq 0] ; then
     echo "Given number $n is even number"
 else
-   echo "Given number $n is odd number"
+    echo "Given number $n is odd number"
 fi
 
 
