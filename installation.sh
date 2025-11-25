eb44a888720c17d3311111a9eaedb0610e32802e
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 19:08:46 +0000

    install my sql server

--- installation.sh
+++ installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USER_ID =$(id -u)
+USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
    echo "ERROR:Please run this script with root previleges"
    exit 1
@@ -7,7 +7,7 @@ fi
 
 dnf install mysql-server -y
 
-if[ $? -eq 0 ] ; then
+if [ $? -eq 0 ] ; then
    echo "installing mysql server is success"
 else
    echo "installing mysql server is failure"
