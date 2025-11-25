0f04c608168f4e8633fbf7e9e697ef21e5abab02
chandanarebba <chandanarebba111@gmail.com>
2025-11-25 18:02:34 +0000

    if condition

--- if-con_1.sh
+++ if-con_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "please enter the number"
 read n
-if[$(($n % 2 -eq 0))] then
+if[$(($n%2 -eq 0))] then
     echo "Given number $n is even number"
 else
    echo "Given number $n is odd number"
